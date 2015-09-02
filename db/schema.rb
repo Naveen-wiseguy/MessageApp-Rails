30d55e3b9e5825782b5a16b5984b3dc753c8ddc3
Naveen <none.wiseguy@gmail.com>
2015-09-02 16:23:49 +0530

    Schema completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150810163450) do
+ActiveRecord::Schema.define(version: 20150902104927) do
+
+  create_table "messages", force: :cascade do |t|
+    t.integer  "sender_id",    limit: 4
+    t.integer  "recipient_id", limit: 4
+    t.text     "body",         limit: 65535
+    t.datetime "sent_at"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name",       limit: 255
